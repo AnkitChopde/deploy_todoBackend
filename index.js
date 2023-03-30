0235e86fd8ea6eeb21beeb10d21e45bb834fd41f
Ankesh Chopde <acchopde84@gmail.com>
2023-03-30 14:29:51 +0530

    backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const authenticate = require("./middlewares/auth.middleware");
 const todoRouter = require("./routes/todo.routes");
 const app = express();
 const userRouter = require("./routes/user.routes")
-
+app.use(cors());
 app.use(express.json());
 // app.use(authenticate)
 todoRouter.use(authenticate)
@@ -17,6 +17,10 @@ app.use("/todos",todoRouter)
 app.get("/",(req,res)=>{
     res.send("Home Page")
 });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'POST,GET,LINK');
+  });
 
 app.listen(process.env.port,async(req,res)=>{
     try{
